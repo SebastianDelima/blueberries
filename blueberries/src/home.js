f7556d7f20b76dc4af123da38611bf4ad93df5bd
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-04-24 21:01:54 -0400

    added true wave

--- blueberries/src/home.js
+++ blueberries/src/home.js
@@ -15,6 +15,13 @@ class Home extends Component {
                     Blueberries
                     </p>
                     <img id="blueImg" src={blue} alt="no img"/>
+                    <svg id="curveSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="white" fill-opacity="1" 
+                    d="M0,224L60,186.7C120,149,240,75,360,90.7C480,107,
+                    600,213,720,213.3C840,213,960,107,1080,69.3C1200,32,1320,
+                    64,1380,80L1440,96L1440,320L1380,320C1320,320,1200,320,1080,
+                    320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                    </svg>
                 </div>
             </header>
             <main>
