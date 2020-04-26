4e67b41281d8d449bb78a3683c7989c7fe4b97a5
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-04-25 21:17:35 -0400

    finished recipes

--- blueberries/src/recipes.js
+++ blueberries/src/recipes.js
@@ -2,6 +2,9 @@ import  React, { Component, Fragment} from 'react';
 import  blue                          from './blueberries.png'
 import  arrow                          from './arrow.svg'
 import  bowl                           from './blueBowl.png'
+import  cake                           from './littleCake.png'
+import  gran                           from './gran.png'
+
 class Recipes extends Component{
     render(){
         return(
@@ -19,12 +22,12 @@ class Recipes extends Component{
                 </header>
                <main>
                  <section id="recipe1">
-                     <p id="recipe1Title">Blueberry Smoothie Bowl</p>
 
+                     <p id="recipe1Title">Blueberry Smoothie Bowl</p>
                      <div className="tables">
                          <div id="ing" className="recipeText">
+
                         <p className="genericSub">Ingredients</p>
-                        
                         <p>- 1 Frozen Bannana</p>
                         <p>- 1 Cup fresh blueberries</p>
                         <p>- 1/2 Cup fresh blackberries</p>
@@ -48,8 +51,82 @@ class Recipes extends Component{
                      <img id="bowl" src={bowl} alt="blue"/>
                      </section> 
 
-                 <div id="recipe2"></div> 
-                 <div id="recipe3"></div> 
+                    <section id="recipe2">
+                    <p id="recipe2Title">Mini Bundt Cakes</p>
+
+                     <div className="tables">
+                         <div id="recipe2Text1">
+                                <p className="genericSub">Ingredients</p>
+                                <p>- 1/4 cup butter, softened</p>
+                                <p>- 1/2 cup sugar</p>
+                                <p>- 1 large egg</p>
+                                <p>- 1/4 cup 2% milk</p>
+                                <p>- 1/2 teaspoon vanilla extract</p>
+                                <p>- 1 cup all-purpose flour</p>
+                                <p>- 1 teaspoon baking powder</p>
+                                <p>1/4 teaspoon salt</p>
+                                <p>1-1/2 cups fresh or frozen blueberries</p>
+                                <p>LEMON ICING:</p>
+                                <p>1/2 cup confectioners' sugar</p>
+                                <p>1-1/2 teaspoons 2% milk</p>
+                                <p>1 teaspoon lemon juice</p>
+                                <p>Additional blueberries, optional</p>
+                         </div>
+                         
+
+                         <div id="recipe2Text2">
+                             <p className="genericSub">Instructions</p>
+                             <p>1. In a small bowl, cream butter and sugar. 
+                                    Beat in the egg, milk and vanilla. 
+                                    Combine the flour, baking powder and salt; stir into creamed mixture. Fold in blueberries.</p><br></br>
+                             <p>2. Pour into three 4-in. fluted tube pans coated with cooking spray.
+                                  Bake at 350° for 25-30 minutes or until a toothpick inserted in the center 
+                                  comes out clean. Cool for 10 minutes before removing from pans to wire racks to cool completely.</p><br></br>
+                             <p>3. For icing, in a small bowl, combine the confectioners' sugar, milk and lemon juice; 
+                                 drizzle over cakes. Garnish with additional berries if desired.</p>
+                         </div> 
+                     </div>
+                     
+                     <img id="cake" src={cake} alt="blue"/>
+                     <img id="cake2" src={cake} alt="blue"/>
+                     </section> 
+
+                 <section id="recipe3">
+                 <p id="recipe1Title">Blueberry Granola</p>
+                     <div className="tables">
+                         <div id="ing" className="recipeTextThree">
+
+                        <p className="genericSub">Ingredients</p>
+                        <p>- 1 cup dried blueberries</p>
+                        <p>- 2 cups  oats </p>
+                        <p>- 1/2 cup sliced almonds</p>
+                        <p>- 1/2 tsp cinnamon</p>
+                        <p>- pinch of salt</p>
+                        <p>- 1/4 cup maple syrup</p>
+                        <p>- 2 tsp vanilla extract</p>
+                        <p>- 3 tbsp coconut oil</p>
+                        
+                         </div>
+                         <div id="rec2" className="recipeText3">
+
+                             <p className="genericSub">Instructions</p>
+                             <div id="weirdP">
+                             <p>1. Preheat the oven to 300 degrees F and line a baking pan with parchment paper.</p>
+                             <p>2. Mix the oats, sliced almonds, cinnamon, and salt together in a bowl.</p>
+                             <p>3. Mix the maple syrup, vanilla extract and coconut oil together in a separate bowl.</p>
+                             <p>4. Pour the liquid into the bowl of dry ingredients and mix together until everything is evenly coated.</p>
+                             <p>5. Spread out evenly on the baking pan and bake for 25-30 minutes, until lightly toasted. Let cool then mix in the dried blueberries.</p>
+                             </div>
+
+                         </div>
+                         
+                     </div>
+                  
+
+                     <img id="gran" src={gran} alt="gran"/>
+                     
+                     
+                     </section> 
                </main>
             </Fragment>
         )
